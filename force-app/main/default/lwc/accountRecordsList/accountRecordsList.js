c607f02d052a5dc0e93d4947ea5a137904418e68
kavisalesforce <kavisalesforce@gmail.com>
2021-10-18 10:59:03 +0800

    Comments Included

--- force-app/main/default/lwc/accountRecordsList/accountRecordsList.js
+++ force-app/main/default/lwc/accountRecordsList/accountRecordsList.js
@@ -1,3 +1,7 @@
+/*
+Display Doctor accounts in lightning home page
+To fetch doctor patient appointment from Events for each doctor
+*/
 import { LightningElement,wire } from 'lwc';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
 export default class AccountRecordsList extends LightningElement {
